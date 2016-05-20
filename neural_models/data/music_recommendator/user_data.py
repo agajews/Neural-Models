8585ade6dd18a49945834f019860f888d07354a7
Alex Gajewski <agajews@gmail.com>
2016-05-20 15:36:46 -0500

    audio_model

--- neural_models/data/music_recommendator/user_data.py
+++ neural_models/data/music_recommendator/user_data.py
@@ -170,7 +170,8 @@ def gen_audio_dataset(num_truncated_songs=10000, num_mels=24):
         fnm = 'raw_data/music_recommendator/audio/' + song + '.mp3.wav'
         if isfile(fnm):
             rate, wav = wavfile.read(fnm)
-            wav = signal.resample(wav, int(len(wav) * 0.25))
+            downsampled_size = int(len(wav) * 0.25)
+            wav = signal.resample(wav, downsampled_size)
             if len(wav.shape) == 2:
                 # print(song)
                 # print(wav)
