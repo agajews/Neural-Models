4a6c0a78439072b939f1831121363d7b4fb6e5e7
Alex Gajewski <agajews@gmail.com>
2016-05-23 10:40:26 -0500

    audio_model

--- neural_models/music_recommendator/audio_model.py
+++ neural_models/music_recommendator/audio_model.py
@@ -305,7 +305,7 @@ def gen_song_data_np(songs_list):
 
         if not isfile(song_wav_fnm):
             download(song['name'], '', song['name'])
-            call('sox %s %s' % (song_fnm, song_wav_fnm))
+            call('sox %s %s' % (song_fnm, song_wav_fnm), shell=True)
 
         wav = get_wav(song_wav_fnm)
 
