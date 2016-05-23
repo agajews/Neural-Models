18a2a8f643608fb9b69613408e1e0f775f1c8be4
Alex Gajewski <agajews@gmail.com>
2016-05-23 12:10:53 -0500

    audio_model

--- neural_models/music_recommendator/audio_model.py
+++ neural_models/music_recommendator/audio_model.py
@@ -304,7 +304,7 @@ def gen_song_data_np(songs_list):
         song_wav_fnm = shlex.quote(song_wav_fnm)
 
         if not isfile(song_wav_fnm):
-            download(song['name'], '', song_fnm)
+            download(song['name'], '', shlex.quote(song['name']))
             call('sox %s %s' % (song_fnm, song_wav_fnm), shell=True)
 
         wav = get_wav(song_wav_fnm)
