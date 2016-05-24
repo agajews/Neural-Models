1bddaa0f47467f7f96ff354c27d01a9a09f5ac6b
Alex Gajewski <agajews@gmail.com>
2016-05-24 12:08:46 -0500

    audio_model

--- neural_models/music_recommendator/audio_model.py
+++ neural_models/music_recommendator/audio_model.py
@@ -53,7 +53,7 @@ class AudioModel(RegressionModel):
     def load_default_data_hyperparams(self):
 
         self.bitwidth = 3
-        self.num_songs = 3300
+        self.num_songs = 3200
 
     def load_default_net_hyperparams(self):
 
