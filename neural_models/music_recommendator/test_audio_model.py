2884126370b5805a49bb2eebe6722b7ec9c92571
Alex Gajewski <agajews@gmail.com>
2016-05-24 12:55:26 -0500

    audio_model

--- neural_models/music_recommendator/test_audio_model.py
+++ neural_models/music_recommendator/test_audio_model.py
@@ -319,7 +319,7 @@ def test_pref_embedding():
     model.build_std_pred_fn()
     model.load_params()
 
-    song_data_np = gen_song_data_np(sam_songs_list)
+    '''song_data_np = gen_song_data_np(sam_songs_list)
 
     song_embeddings = gen_song_embeddings(model, song_data_np)
 
@@ -331,12 +331,12 @@ def test_pref_embedding():
     user_preds = get_user_preds(model, user_prefs, all_song_embeddings)
     user_preds = sorted(user_preds, key=lambda k: k['exp_play_count'], reverse=False)
 
-    print(user_preds[:10])
+    print(user_preds[:10])'''
 
-    '''user_songs, user_counts = gen_user_data_np(songs_list)
+    user_songs, user_counts = gen_user_data_np(alex_songs_list)
     input_song_fnm = 'raw_data/music_recommendator/audio/' + 'SOAATLI12A8C13E319.mp3.wav'
     user_preds = get_std_user_preds(model, user_songs, user_counts, input_song_fnm)
-    print(user_preds)'''
+    print(user_preds)
 
 
 def main():
