7a4e8642c2490d62fe87c1b2eb63980760f55f66
Alex Gajewski <agajews@gmail.com>
2016-05-27 14:18:34 -0500

    Bumped version

--- neural_models/music_recommendator/test_audio_model.py
+++ neural_models/music_recommendator/test_audio_model.py
@@ -13,7 +13,6 @@ from os.path import isfile
 from subprocess import call
 
 from neural_models.data.music_recommendator.user_data import download
-from neural_models.data.music_recommendator.lib import User, Song
 from neural_models.music_recommendator.audio_model import AudioModel
 
 
@@ -277,6 +276,47 @@ wanqi_songs_list = [
             'song_id': 'u4s12'
         }
 ]
+
+
+class Song(object):
+
+    def __init__(self, song_id, name=None, artist=None, play_count=None):
+
+        self.song_id = song_id
+        self.name = name
+        self.artist = artist
+        self.play_count = play_count
+
+
+class User(object):
+
+    def __init__(self, user_id, songs=None):
+
+        self.user_id = user_id
+        self.hist = songs
+        self.prefs = None
+
+    def __repr__(self):
+
+        return 'User(%d)' % self.user_id
+
+    def add_wavs(self):
+
+        create_wavs(self.hist)
+        add_wavs(self.hist)
+
+        for song in self.hist:
+            if song.wav is None:
+                self.hist.remove(song)
+
+    def add_embeddings(self, model):
+
+        for song in self.hist:
+            song.embedding = model.get_song_embedding(song.wav)
+
+    def add_filenames(self):
+
+        add_filenames(self.hist)
 
 
 def add_wav(song):
